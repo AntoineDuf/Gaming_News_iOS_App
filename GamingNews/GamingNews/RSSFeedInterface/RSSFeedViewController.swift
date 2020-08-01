5570fa584d62f85c990cf8985dbe2e70dba833c9
AntoineDuf <antoine.dufayet@gmail.com>
2020-08-01 15:23:15 +0200

    Create RSSFeedViewController file.

---
+++ GamingNews/GamingNews/RSSFeedInterface/RSSFeedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RSSFeedViewController.swift
+//  GamingNews
+//
+//  Created by Antoine Dufayet on 01/08/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import UIKit
+
+class RSSFeedViewController: UITableViewController {
+    var viewModel: RSSFeedViewModel!
+}
