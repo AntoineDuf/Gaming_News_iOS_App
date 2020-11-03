32e21c7253f48feed3ef3a7f8dee4728fea19944
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:51:16 +0100

    Creation of TabBarViewModel.

---
+++ GamingNews/GamingNews/TabBar/TabBarViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarViewModel.swift
+//  GamingNews
+//
+//  Created by Antoine Dufayet on 01/10/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import Foundation
+
+class TabBarViewModel {
+    var selectedItem = 0
+}
