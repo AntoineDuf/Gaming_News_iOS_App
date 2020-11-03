6fa86fe94b3131e0e4068003021471cb70931d9b
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:52:41 +0100

    Refacto Thematic.swift file. Add one properties and conform to Equatable protocol.

--- GamingNews/GamingNews/Thematic.swift
+++ GamingNews/GamingNews/Thematic.swift
@@ -5,17 +5,26 @@
 //  Created by Antoine Dufayet on 25/07/2020.
 //  Copyright © 2020 NatProd. All rights reserved.
 //
+//swiftlint:disable identifier_name
 
 import Foundation
 
 struct Thematic {
     let id: Int
+    let imageName: String
     let name: String
     let documentId: String
-    
-    init(id: Int, name: String, documentId: String) {
+
+    init(id: Int, imageName: String, name: String, documentId: String) {
         self.id = id
+        self.imageName = imageName
         self.name = name
         self.documentId = documentId
+    }
+}
+
+extension Thematic: Equatable {
+    static func == (lhs: Thematic, rhs: Thematic) -> Bool {
+        return lhs.id == rhs.id
     }
 }
