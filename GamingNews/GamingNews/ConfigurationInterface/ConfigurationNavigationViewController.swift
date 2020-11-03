051cea4c8ab11adb81e2dcfef53c9bb6e963911a
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:31:43 +0100

    Creation of ConfigurationNavigationViewController file.

---
+++ GamingNews/GamingNews/ConfigurationInterface/ConfigurationNavigationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ConfigurationNavigationViewController.swift
+//  GamingNews
+//
+//  Created by Antoine Dufayet on 24/09/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import UIKit
+
+class ConfigurationNavigationViewController: UINavigationController {
+}
