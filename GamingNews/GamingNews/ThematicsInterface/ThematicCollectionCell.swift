f0449253034008ec30df4c6bec1d6f32abf4eb1c
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:53:18 +0100

    Refacto of ThematicCollectionCell and add configureStyleCell method.

--- GamingNews/GamingNews/ThematicsInterface/ThematicCollectionCell.swift
+++ GamingNews/GamingNews/ThematicsInterface/ThematicCollectionCell.swift
@@ -6,19 +6,24 @@
 //  Copyright © 2020 NatProd. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import FirebaseUI
 
-
 class ThematicCollectionCell: UICollectionViewCell {
     @IBOutlet private weak var imageCell: UIImageView!
-    
+
+    override func awakeFromNib() {
+         configureStyleCell()
+     }
+
     func configureCell(thematic: Thematic) {
         let storage = Storage.storage()
         let storageReference = storage.reference()
-        
-        let reference = storageReference.child("\(thematic.name)")
+        let reference = storageReference.child("\(thematic.imageName)")
         imageCell.sd_setImage(with: reference)
     }
+
+     private func configureStyleCell() {
+         self.layer.cornerRadius = 8
+     }
 }
