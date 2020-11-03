1cbda7f33acc329c64b4e702100d03b2a14d68af
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:48:32 +0100

    Extension of RSSItem to conform Hashable and equatable protocols.

---
+++ GamingNews/GamingNews/Extensions/RSSItem+Equatable.swift
@@ -0,0 +1,24 @@
+//
+//  RSSItem+Equatable.swift
+//  GamingNews
+//
+//  Created by Antoine Dufayet on 22/10/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import AlamofireRSSParser
+
+extension RSSItem: Hashable, Equatable {
+    public static func == (lhs: RSSItem, rhs: RSSItem) -> Bool {
+        return lhs.title == rhs.title
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
+extension Array where Element: Hashable {
+    var unique: [Element] {
+        return Array(Set(self))
+    }
+}
