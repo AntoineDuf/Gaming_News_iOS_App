d19efecbbb31dce074e6aaa955d5ca0f0c71c7af
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:46:33 +0100

    Prepare the segue add of life cycle of the table view and configure the viewModel handlers.

--- GamingNews/GamingNews/RSSFeedInterface/RSSFeedViewController.swift
+++ GamingNews/GamingNews/RSSFeedInterface/RSSFeedViewController.swift
@@ -5,14 +5,66 @@
 //  Created by Antoine Dufayet on 01/08/2020.
 //  Copyright © 2020 NatProd. All rights reserved.
 //
+//swiftlint:disable identifier_name
 
 import UIKit
 
 class RSSFeedViewController: UITableViewController {
     var viewModel: RSSFeedViewModel!
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        print(viewModel.articles[0])
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewModel()
+        DispatchQueue.main.async {
+            self.viewModel.getRSSLinks()
+        }
+    }
+}
+
+extension RSSFeedViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let webVC = segue.destination as? WebViewController,
+            let article = viewModel.selectedArticle {
+            webVC.viewModel = WebViewModel(article: article)
+        }
+    }
+}
+
+extension RSSFeedViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.articles.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "\(ArticleTableViewCell.self)",
+            for: indexPath
+            ) as? ArticleTableViewCell else { fatalError() }
+        let article = viewModel.articles[indexPath.row]
+        cell.configureCell(article: article, thematic: viewModel.thematic)
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return (view.frame.height / 6)
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        DispatchQueue.main.async {
+            self.viewModel.didSelectArticle(at: indexPath)
+        }
+    }
+}
+
+private extension RSSFeedViewController {
+    func configureViewModel() {
+        viewModel.articleHandler = { [weak self] in
+            guard let me = self else { return }
+            me.performSegue(withIdentifier: StoryboardSegue.RSSFeedStoryboard.showWebView.rawValue, sender: nil)
+        }
+        viewModel.articlesHandler = { [weak self] in
+            guard let me = self else { return }
+                me.tableView.reloadData()
+        }
     }
 }
