d0fceaf258e9cc331a2b498515bf5ced0c721211
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:27:45 +0100

    Creation of item structure.

---
+++ GamingNews/GamingNews/Article.swift
@@ -0,0 +1,15 @@
+//
+//  Article.swift
+//  GamingNews
+//
+//  Created by Antoine Dufayet on 14/08/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import Foundation
+
+struct Item: Codable {
+    var title: String
+    var link: String
+    var body: String
+}
