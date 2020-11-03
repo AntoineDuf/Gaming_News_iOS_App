425c1abc2b1e4325ca6b4be828074a9c0ccd0fc8
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 19:35:47 +0100

    Creation of ArticleTableViewCell file and add of IBOutlets and  configureCell , displayNewsDelay methods.

--- GamingNews/GamingNews/RSSFeedInterface/ArticleTableViewCell.swift
+++ GamingNews/GamingNews/RSSFeedInterface/ArticleTableViewCell.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2020 NatProd. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AlamofireRSSParser
+import AlamofireImage
+import FirebaseUI
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var sourceLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var articleImage: UIImageView!
+
+    func configureCell(article: RSSItem, thematic: Thematic) {
+        label.text = article.title
+        sourceLabel.text = article.source
+        displayNewsDelay(article: article)
+        guard let image = article.mediaThumbnail else {
+            guard let image = article.enclosures?[0] else {
+                let storage = Storage.storage()
+                let storageReference = storage.reference()
+                let reference = storageReference.child("\(thematic.name)")
+                articleImage.sd_setImage(with: reference)
+                return
+            }
+            articleImage.af.setImage(withURL: URL(string: image["url"]!)!)
+            return
+        }
+        articleImage.af.setImage(withURL: URL(string: image)!)
+        return
+    }
+
+    private func displayNewsDelay(article: RSSItem) {
+        var calendar = NSCalendar.autoupdatingCurrent
+        calendar.timeZone = NSTimeZone.system
+        if let startDate = article.pubDate {
+            let components = calendar.dateComponents(
+                [ .hour, .day ],
+                from: startDate,
+                to: NSDate() as Date
+            )
+            if let hour = components.hour,
+                let day = components.day {
+                if day > 0 {
+                    timeLabel.text = "\(String(describing: day)) jour(s)"
+                } else {
+                    timeLabel.text = "\(String(describing: hour)) heure(s)"
+                }
+            }
+        }
+    }
+}
