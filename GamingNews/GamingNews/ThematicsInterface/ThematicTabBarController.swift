ebed2a2a146f5a8da6c72007885d70cba4cff6e9
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-03 20:54:05 +0100

    Creation of ThematicTabBarController.

---
+++ GamingNews/GamingNews/ThematicsInterface/ThematicTabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  ThematicTabBarController.swift
+//  GamingNews
+//
+//  Created by Antoine Dufayet on 09/09/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import UIKit
+
+class ThematicTabBarController: UINavigationController {
+}
