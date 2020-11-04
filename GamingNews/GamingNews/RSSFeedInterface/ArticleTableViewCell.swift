78b7a5119fab052400fbbab8daccc773b755c7d9
AntoineDuf <antoine.dufayet@gmail.com>
2020-11-04 19:02:04 +0100

    ArticleTableViewCell.swift: Refacto in configureCell method, hide the image if it is not available. Refacto displayNewsDelay method to display minutes.

--- GamingNews/GamingNews/RSSFeedInterface/ArticleTableViewCell.swift
+++ GamingNews/GamingNews/RSSFeedInterface/ArticleTableViewCell.swift
@@ -16,20 +16,16 @@ class ArticleTableViewCell: UITableViewCell {
     @IBOutlet private weak var sourceLabel: UILabel!
     @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var articleImage: UIImageView!
-
+    @IBOutlet weak var imageStackView: UIView!
+    
     func configureCell(article: RSSItem, thematic: Thematic) {
+        articleImage.layer.cornerRadius = 8
         label.text = article.title
         sourceLabel.text = article.source
         displayNewsDelay(article: article)
         guard let image = article.mediaThumbnail else {
-            guard let image = article.enclosures?[0] else {
-                let storage = Storage.storage()
-                let storageReference = storage.reference()
-                let reference = storageReference.child("\(thematic.name)")
-                articleImage.sd_setImage(with: reference)
-                return
-            }
-            articleImage.af.setImage(withURL: URL(string: image["url"]!)!)
+            articleImage.isHidden = true
+            imageStackView.isHidden = true
             return
         }
         articleImage.af.setImage(withURL: URL(string: image)!)
@@ -41,16 +37,19 @@ class ArticleTableViewCell: UITableViewCell {
         calendar.timeZone = NSTimeZone.system
         if let startDate = article.pubDate {
             let components = calendar.dateComponents(
-                [ .hour, .day ],
+                [ .hour, .day, .minute ],
                 from: startDate,
                 to: NSDate() as Date
             )
             if let hour = components.hour,
-                let day = components.day {
+                let day = components.day,
+                let minute = components.minute {
                 if day > 0 {
-                    timeLabel.text = "\(String(describing: day)) jour(s)"
-                } else {
-                    timeLabel.text = "\(String(describing: hour)) heure(s)"
+                    timeLabel.text = "Il y a \(String(describing: day)) jour(s)"
+                } else if minute < 60 && hour < 1 {
+                    timeLabel.text = "Il y a \(String(describing: minute)) minute(s)"
+                    } else {
+                        timeLabel.text = "Il y a \(String(describing: hour)) heure(s) & \(String(describing: minute)) minute(s)"
                 }
             }
         }
