393a18b1d179641e5b969e53ddd59a267e9bac3c
AntoineDuf <antoine.dufayet@gmail.com>
2020-12-07 11:38:41 +0100

    Some clean in the app delegate.

--- GamingNews/GamingNews/AppDelegate.swift
+++ GamingNews/GamingNews/AppDelegate.swift
@@ -8,11 +8,9 @@
 
 import UIKit
 import FirebaseCore
-import GoogleSignIn
-import FBSDKCoreKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
@@ -22,45 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Override point for customization after application launch.
-//        FirebaseApp.configure()
-        GIDSignIn.sharedInstance()?.delegate.self
-        GIDSignIn.sharedInstance()?.clientID = "207783282282-28virf6jtvgkesur95hms422h0slfrst.apps.googleusercontent.com"
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = AppRouter.shared.makeFirstVC()
-        window?.makeKeyAndVisible()
         return true
-    }
-
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
-              withError error: Error!) {
-        if let error = error {
-            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
-                print("The user has not signed in before or they have since signed out.")
-            } else {
-                print("\(error.localizedDescription)")
-            }
-            return
-        }
-    }
-//
-//    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
-//              withError error: Error!) {
-//        // Perform any operations when the user disconnects from app here.
-//        // ...
-//    }
-
-    func application(
-        _ app: UIApplication,
-        open url: URL,
-        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-        ApplicationDelegate.shared.application(
-            app,
-            open: url,
-            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-        )
-        return GIDSignIn.sharedInstance().handle(url)
     }
 }
